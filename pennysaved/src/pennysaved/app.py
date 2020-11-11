9ae2fe49bdf8f347d4061d6148f6656ed0a8166a
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-11-11 15:38:01 -0800

    Update app.py

--- pennysaved/src/pennysaved/app.py
+++ pennysaved/src/pennysaved/app.py
@@ -2,29 +2,31 @@ import pymysql.cursors
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-
+import time
 
 class APennySaved(toga.App):
 
     def startup(self):
-
-        main_box = toga.Box()
-        LoginB = toga.Button('Login', on_press=Login(main_box, LoginB, SignupB))
-        SignupB = toga.Button('Signup', on_press=Signup(main_box, LoginB, SignupB))
-        main_box.add(LoginB)
-        main_box.add(SignupB)
-
-
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
+    	main_box = toga.Box()
+    	self.main_window = toga.MainWindow(title=self.formal_name)
+    	self.main_window.content = main_box
+    	self.main_window.show()
         
 
+def start():
+	main_box = toga.Box()
+	LoginB = toga.Button('Login', on_press=Login)
+	SignupB = toga.Button('Signup', on_press=Signup)
+	main_box.add(LoginB)
+	main_box.add(SignupB)
+	main_window = toga.MainWindow(title="formal_name")
+	main_window.content = main_box
+	main_window.show()
 
-def Login(main_box, LoginB, SignupB):
-	main_box.remove(LoginB)
 
-def Signup(main_box, LoginB, SignupB):
+def Login():
+	print("Login")
+def Signup():
 	print("Signup")
 
 def main():
@@ -37,3 +39,5 @@ connection = pymysql.connect(host='johnny.heliohost.org',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+
+start()
